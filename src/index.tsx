2d8ce9e5ac057fd22e2eda148ec5febd53f514a3
tranceyos2419 <tranceyos2419@gmail.com>
2020-03-23 13:30:02 +0900

    add: added row logRockt by following the getting started instruction

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LogRocket from 'logrocket';
+LogRocket.init('xirvnf/firebase-test');
 
 ReactDOM.render(
   <React.StrictMode>
